5e487f3484b86b258529aeab6a1f9570582541f3
kdduha <just.andrew.kd@gmail.com>
2025-05-05 00:52:35 +0300

    add monitoring

--- src/config.py
+++ src/config.py
@@ -13,6 +13,7 @@ class UvicornConfig(ConfigBase):
     port: int = Field(8000)
     workers: int | None = Field(None)
     log_level: str = Field("info")
+    reload: bool = Field(False)
 
 
 class DataBaseConfig(ConfigBase):
@@ -30,6 +31,12 @@ class DataBaseConfig(ConfigBase):
         return f"postgresql://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
 
 
+class PrometheusConfig(ConfigBase):
+    model_config = SettingsConfigDict(env_prefix="PR_")
+
+    monitor: bool = Field(False)
+
+
 class AuthConfig(ConfigBase):
     model_config = SettingsConfigDict(env_prefix="AUTH_")
 
@@ -42,6 +49,7 @@ class Config(ConfigBase):
     uvicorn: UvicornConfig = Field(default_factory=UvicornConfig)
     db: DataBaseConfig = Field(default_factory=DataBaseConfig)
     auth: AuthConfig = Field(default_factory=AuthConfig)
+    prometheus: PrometheusConfig = Field(default_factory=PrometheusConfig)
 
     @classmethod
     def load(cls) -> "Config":
